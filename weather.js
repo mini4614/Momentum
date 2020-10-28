7a4bd0a5dc42398031ed6b0ec9aaac5713e1cd6d
mini4614 <mini4614@gmail.com>
2020-10-29 01:38:37 +0900

    완성 커밋

--- weather.js
+++ weather.js
@@ -7,7 +7,7 @@ function getWeather(lat, lng) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`).then(function(response){
         return response.json();
     }).then(function(json){
-        console.log(json);
+ 
         const temperature = json.main.temp;
         const place = json.name;
 
@@ -47,7 +47,7 @@ function loadCoords() {
     } else {
         //get weather
         const parseCoords = JSON.parse(loadedCoords);
-        console.log(parseCoords);
+       
         getWeather(parseCoords.latitude,parseCoords.longitude);
     }
 }
